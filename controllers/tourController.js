0d4c904edd9d3ec2e4af2d4dd58b58fd59afd484
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-02-02 01:57:30 +0100

    Adding alias route for top 5 cheapest tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,13 @@
 const Tour = require("./../models/tourModel");
 
-// ROUTE HANDLERS functions
+exports.aliasTopTours = async (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD THE QUERY
