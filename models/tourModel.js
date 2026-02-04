1c2de424515b5cfc8853ac0d935dfbd885f55487
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-02-04 23:03:39 +0100

    Adding Duration in weeks features using virtual properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -52,7 +52,14 @@ const tourShema = new mongoose.Schema({
     select: false,
   },
   startDates: [Date],
+}, {
+  toJSON: {virtuals: true},
+  toObject: {virtuals: true}
 });
+
+tourShema.virtual("durationWeeks").get(function() {
+  return this.duration / 7
+})
 
 const Tour = mongoose.model("Tour", tourShema);
 
